f21d0ee10eb6531712fbe6252b8e28414133e4fe
Jaden Falbee <j.falbee04@gmail.com>
2023-06-19 22:07:11 -0400

    changes to index, script, and style. finishes

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+// const h2 = document.createElement("h2");
+// h2.textContent = "This content added by JavaScript";
+// document.querySelector("body").appendChild(h2);
+
+// const h1 = document.createElement("h1");
+// h1.textContent = "Made by: Jaden Falbee"
+// document.querySelector("head").appendChild(h1)
+
+const h7 = document.createElement("h7");
+ h7.textContent = "Pics";
+document.querySelector("body").appendChild(h7);
